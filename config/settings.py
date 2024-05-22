884851110d9048f7023e95608d4b258fd8941805
Navneet <yadavnavneet018@gmail.com>
2024-05-22 15:28:43 +0530

    change

--- config/settings.py
+++ config/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.digipodium.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "navneetyadv6034@digipodium.com"
+EMAIL_HOST_PASSWORD = "digi60342k23"
 DEFAULT_FROM_EMAIL = "navneetyadv6034@digipodium.com"
 # Other 3rd party settings
 
